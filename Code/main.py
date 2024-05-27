ae9adfe815d3e6e67a9cc088690c7975ca80324f
Matías Villarreal Cruz <matiasvillarrealcruz@Laptop-de-Matias.local>
2024-05-26 22:27:47 -0500

    Sin CodeSmell

--- Code/main.py
+++ Code/main.py
@@ -1,199 +1,204 @@
-#––––––––– Clase Album –––––––––––
+# ––––––––– Clase Album –––––––––––
 class Album:
-    def __init__(self, nombre, anioLanzamiento, disquera, artistas):
+    def __init__(self, nombre, anio_lanzamiento, disquera, artistas):
         self.nombre = nombre
-        self.anioLanzamiento = anioLanzamiento
+        self.anio_lanzamiento = anio_lanzamiento
         self.disquera = disquera
         self.artistas = artistas  # Lista de objetos Artista
         self.canciones = []  # Lista de objetos Cancion
 
-    def agregarCancion(self, cancion):
-        if not self.verificarTituloRepetido(cancion.titulo):
+    def agregar_cancion(self, cancion):
+        if not self.verificar_titulo_repetido(cancion.titulo):
             self.canciones.append(cancion)
         else:
-            raise ValueError("La cancion con el titulo '{}' ya existe en el album.".format(cancion.titulo))
+            raise ValueError("La canción con el título '{}' ya existe en el álbum.".format(cancion.titulo))
 
-    def verificarTituloRepetido(self, titulo):
+    def verificar_titulo_repetido(self, titulo):
         return any(cancion.titulo == titulo for cancion in self.canciones)
 
-    def listarCanciones(self):
+    def listar_canciones(self):
         return [(cancion.titulo, cancion.duracion) for cancion in self.canciones]
 
-###––––––––––– Clase Cancion ––––––––––
+# ––––––––––– Clase Cancion ––––––––––
 class Cancion:
     def __init__(self, titulo, duracion):
         self.titulo = titulo
         self.duracion = duracion
 
-
-###–––––––––– Clase Artista –––––––––––
+# –––––––––– Clase Artista –––––––––––
 class Artista:
-    def __init__(self, nombre, paisOrigen):
+    def __init__(self, nombre, pais_origen):
         self.nombre = nombre
-        self.paisOrigen = paisOrigen
+        self.pais_origen = pais_origen
 
-###––––––––– Clase MusicTracker –––––––––––––––
+# ––––––––– Clase MusicTracker –––––––––––––––
 class MusicTracker:
     def __init__(self):
         self.albumes = []  # Lista de objetos Album
 
-    def agregarAlbum(self, nombre, anioLanzamiento, disquera, artistas):
-        nuevoAlbum = Album(nombre, anioLanzamiento, disquera, artistas)
-        self.albumes.append(nuevoAlbum)
-        return nuevoAlbum
+    def agregar_album(self, nombre, anio_lanzamiento, disquera, artistas):
+        nuevo_album = Album(nombre, anio_lanzamiento, disquera, artistas)
+        self.albumes.append(nuevo_album)
+        return nuevo_album
 
-    def agregarCancionAlbum(self, nombreAlbum, tituloCancion, duracion):
-        album = self.buscarAlbumNombre(nombreAlbum)
+    def agregar_cancion_album(self, nombre_album, titulo_cancion, duracion):
+        album = self.buscar_album_nombre(nombre_album)
         if album:
-            nuevaCancion = Cancion(tituloCancion, duracion)
-            album.agregarCancion(nuevaCancion)
+            nueva_cancion = Cancion(titulo_cancion, duracion)
+            album.agregar_cancion(nueva_cancion)
         else:
-            raise ValueError("album no encontrado.")
+            raise ValueError("Álbum no encontrado.")
 
-    def buscarAlbumNombre(self, nombre):
+    def buscar_album_nombre(self, nombre):
         for album in self.albumes:
             if album.nombre == nombre:
                 return album
         return None
 
-    def buscarAlbumesAnio(self, año):
-        return [album for album in self.albumes if album.anioLanzamiento == año]
+    def buscar_albumes_anio(self, anio):
+        return [album for album in self.albumes if album.anio_lanzamiento == anio]
 
-###––––––––––– Clase Main ––––––––––––––
+# ––––––––––– Clase Main ––––––––––––––
+class Main:
+    @staticmethod
+    def mostrar_menu():
+        print("\tMenú de Opciones")
+        print("1. Agregar un nuevo Álbum")
+        print("2. Agregar canción a Álbum Existente")
+        print("3. Ver Canciones de Álbum")
+        print("4. Buscar Álbum por Año")
+        print("5. Salir")
 
-def mostrarMenu():
-    print("\tMenu de Opciones")
-    print("1. Agregar un nuevo Album")
-    print("2. Agregar cancion a Album Existente")
-    print("3. Ver Canciones de Album")
-    print("4. Buscar Album por Anio")
-    print("5. Salir")
+    @staticmethod
+    def obtener_artistas():
+        artistas = []
+        while True:
+            nombre_artista = input("Ingrese el nombre del artista (o 'fin' para terminar): ")
+            if nombre_artista.lower() == 'fin':
+                break
+            pais_origen = input(f"Ingrese el país de origen de {nombre_artista}: ")
+            artistas.append(Artista(nombre_artista, pais_origen))
+        return artistas
 
-def obtenerArtista():
-    artistas = []
-    while True:
-        nombreArtista = input("Ingrese el nombre del artista (o 'fin' para terminar): ")
-        if nombreArtista.lower() == 'fin':
-            break
-        paisOrigen = input(f"Ingrese el país de origen de {nombreArtista}: ")
-        artistas.append(Artista(nombreArtista, paisOrigen))
-    return artistas
+    @staticmethod
+    def guardar_datos(music_tracker):
+        with open("albumes_y_canciones.txt", "w") as file:
+            for album in music_tracker.albumes:
+                file.write(f"Álbum: {album.nombre}\n")
+                file.write(f"Año de Lanzamiento: {album.anio_lanzamiento}\n")
+                file.write(f"Disquera: {album.disquera}\n")
+                file.write(f"Artistas: {', '.join(artista.nombre for artista in album.artistas)}\n")
+                file.write("Canciones:\n")
+                for cancion in album.canciones:
+                    file.write(f"\t- {cancion.titulo} ({cancion.duracion} minutos)\n")
+                file.write("\n")
+        print("Datos guardados en 'albumes_y_canciones.txt'.")
 
-def guardarDatos(musicTracker):
-    with open("albumes_y_canciones.txt", "w") as file:
-        for album in musicTracker.albumes:
-            file.write(f"Álbum: {album.nombre}\n")
-            file.write(f"Año de Lanzamiento: {album.anioLanzamiento}\n")
-            file.write(f"Disquera: {album.disquera}\n")
-            file.write(f"Artistas: {', '.join(artista.nombre for artista in album.artistas)}\n")
-            file.write("Canciones:\n")
-            for cancion in album.canciones:
-                file.write(f"\t- {cancion.titulo} ({cancion.duracion} minutos)\n")
-            file.write("\n")
-    print("Datos guardados en 'albumes_y_canciones.txt'.")
-
-def cargarDatos(nombre_archivo):
-    musicTracker = MusicTracker()
-    
-    try:
-        with open(nombre_archivo, "r") as file:
-            lineas = file.readlines()
-    except FileNotFoundError:
-        return None
-    
-    album_actual = None
-    for linea in lineas:
-        linea = linea.strip()
-        
-        if linea.startswith("Álbum:"):
-            nombre_album = linea[len("Álbum: "):]
-            album_actual = Album(nombre_album, "", "", [])
-            musicTracker.albumes.append(album_actual)
-        
-        elif linea.startswith("Año de Lanzamiento:"):
-            anio_lanzamiento = linea[len("Año de Lanzamiento: "):]
-            album_actual.anioLanzamiento = anio_lanzamiento
-        
-        elif linea.startswith("Disquera:"):
-            disquera = linea[len("Disquera: "):]
-            album_actual.disquera = disquera
-        
-        elif linea.startswith("Artistas:"):
-            artistas_nombres = linea[len("Artistas: "):].split(", ")
-            artistas = [Artista(nombre, "") for nombre in artistas_nombres]
-            album_actual.artistas = artistas
-        
-        elif linea.startswith("- "):
-            partes = linea[2:].split(" (")
-            titulo_cancion = partes[0]
-            duracion_cancion = partes[1].replace(" minutos)", "")
-            nueva_cancion = Cancion(titulo_cancion, duracion_cancion)
-            album_actual.canciones.append(nueva_cancion)
-    
-    if not musicTracker.albumes:  
-        return None
-    
-    print("Datos cargados desde '{}'.".format(nombre_archivo))
-    return musicTracker
-
-
-musicTracker = cargarDatos("albumes_y_canciones.txt")
-if musicTracker is None:
-    musicTracker = MusicTracker()
-flag = True
-while flag:
-    mostrarMenu()
-    opc = input("Seleccione una opcion (numero): ")
-    
-    if not opc.isdigit():
-        print("Por favor, ingrese un numero válido.")
-        continue
-
-    opc = int(opc)
-    
-    if opc == 1:
-        nombre = input("Ingrese el nombre del album: ")
-        anioLanzamiento = input("Ingrese el año de lanzamiento: ")
-        disquera = input("Ingrese el nombre de la disquera: ")
-        artistas = obtenerArtista()
-        musicTracker.agregarAlbum(nombre, anioLanzamiento, disquera, artistas)
-        print("Álbum agregado exitosamente.")
-    
-    elif opc == 2:
-        nombreAlbum = input("Ingrese el nombre del album: ")
-        tituloCancion = input("Ingrese el titulo de la cancion: ")
-        duracion = input("Ingrese la duracion de la cancion (en minutos): ")
+    @staticmethod
+    def cargar_datos(nombre_archivo):
+        music_tracker = MusicTracker()
         try:
-            musicTracker.agregarCancionAlbum(nombreAlbum, tituloCancion, duracion)
+            with open(nombre_archivo, "r") as file:
+                lineas = file.readlines()
+        except FileNotFoundError:
+            return None
+        
+        album_actual = None
+        for linea in lineas:
+            linea = linea.strip()
+            if linea.startswith("Álbum:"):
+                nombre_album = linea[len("Álbum: "):]
+                album_actual = Album(nombre_album, "", "", [])
+                music_tracker.albumes.append(album_actual)
+            elif linea.startswith("Año de Lanzamiento:"):
+                anio_lanzamiento = linea[len("Año de Lanzamiento: "):]
+                album_actual.anio_lanzamiento = anio_lanzamiento
+            elif linea.startswith("Disquera:"):
+                disquera = linea[len("Disquera: "):]
+                album_actual.disquera = disquera
+            elif linea.startswith("Artistas:"):
+                artistas_nombres = linea[len("Artistas: "):].split(", ")
+                artistas = [Artista(nombre, "") for nombre in artistas_nombres]
+                album_actual.artistas = artistas
+            elif linea.startswith("- "):
+                partes = linea[2:].split(" (")
+                titulo_cancion = partes[0]
+                duracion_cancion = partes[1].replace(" minutos)", "")
+                nueva_cancion = Cancion(titulo_cancion, duracion_cancion)
+                album_actual.canciones.append(nueva_cancion)
+        if not music_tracker.albumes:
+            return None
+        print("Datos cargados desde '{}'.".format(nombre_archivo))
+        return music_tracker
+
+    def __init__(self):
+        self.music_tracker = self.cargar_datos("albumes_y_canciones.txt")
+        if self.music_tracker is None:
+            self.music_tracker = MusicTracker()
+
+    def ejecutar(self):
+        flag = True
+        while flag:
+            self.mostrar_menu()
+            opc = input("Seleccione una opción (número): ")
+            if not opc.isdigit():
+                print("Por favor, ingrese un número válido.")
+                continue
+            opc = int(opc)
+            if opc == 1:
+                self.opcion_agregar_album()
+            elif opc == 2:
+                self.opcion_agregar_cancion_album()
+            elif opc == 3:
+                self.opcion_ver_canciones_album()
+            elif opc == 4:
+                self.opcion_buscar_album_anio()
+            elif opc == 5:
+                self.guardar_datos(self.music_tracker)
+                flag = False
+                print("Saliendo del sistema...")
+            else:
+                print("Opción no válida, por favor intente de nuevo.")
+
+    def opcion_agregar_album(self):
+        nombre = input("Ingrese el nombre del álbum: ")
+        anio_lanzamiento = input("Ingrese el año de lanzamiento: ")
+        disquera = input("Ingrese el nombre de la disquera: ")
+        artistas = self.obtener_artistas()
+        self.music_tracker.agregar_album(nombre, anio_lanzamiento, disquera, artistas)
+        print("Álbum agregado exitosamente.")
+
+    def opcion_agregar_cancion_album(self):
+        nombre_album = input("Ingrese el nombre del álbum: ")
+        titulo_cancion = input("Ingrese el título de la canción: ")
+        duracion = input("Ingrese la duración de la canción (en minutos): ")
+        try:
+            self.music_tracker.agregar_cancion_album(nombre_album, titulo_cancion, duracion)
             print("Canción agregada exitosamente.")
         except ValueError as e:
             print(e)
-    
-    elif opc == 3:
-        nombreAlbum = input("Ingrese el nombre del album: ")
-        album = musicTracker.buscarAlbumNombre(nombreAlbum)
+
+    def opcion_ver_canciones_album(self):
+        nombre_album = input("Ingrese el nombre del álbum: ")
+        album = self.music_tracker.buscar_album_nombre(nombre_album)
         if album:
-            canciones = album.listarCanciones()
-            print(f"Canciones en el álbum '{nombreAlbum}':")
+            canciones = album.listar_canciones()
+            print(f"Canciones en el álbum '{nombre_album}':")
             for titulo, duracion in canciones:
                 print(f"- {titulo} ({duracion} minutos)")
         else:
             print("Álbum no encontrado.")
-    
-    elif opc == 4:
+
+    def opcion_buscar_album_anio(self):
         anio = input("Ingrese el año de lanzamiento: ")
-        albumes = musicTracker.buscarAlbumesAnio(anio)
+        albumes = self.music_tracker.buscar_albumes_anio(anio)
         if albumes:
             print(f"Álbumes lanzados en {anio}:")
             for album in albumes:
                 print(f"- {album.nombre} por {', '.join(artista.nombre for artista in album.artistas)}")
         else:
             print("No se encontraron álbumes para el año especificado.")
-    
-    elif opc == 5:
-        guardarDatos(musicTracker)
-        flag = False
-        print("Saliendo del sistema...")
-    
-    else:
-        print("Opción no válida, por favor intente de nuevo.")
+
+if __name__ == "__main__":
+    main = Main()
+    main.ejecutar()
